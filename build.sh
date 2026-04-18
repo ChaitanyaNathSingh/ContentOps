61da1d4b9c7cdbd95687a3d01b2330e12d6ba4ae
ChaitanyaNathSingh <chaitanya.nath@hackerearth.com>
2026-04-18 21:14:50 +0530

    Add superuser creation step in build script. Ensure admin user is created if not already present during deployment setup.

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'chaitanya.nath@hackerearth.com', 'Admin@1234')"
